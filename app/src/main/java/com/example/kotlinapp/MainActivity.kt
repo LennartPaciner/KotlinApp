812db6ed67af10f102ec6cf512ca772c552871f2
lpaciner <lpaciner@ics-group.eu>
2021-08-17 10:38:34 +0200

    gradle und tut begin

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlinapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//tut: https://developer.android.com/codelabs/android-room-with-a-view-kotlin#0
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
