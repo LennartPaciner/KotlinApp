0d9821f8677ccca14595a2aa99fb3787c8fa6451
lpaciner <lpaciner@ics-group.eu>
2021-08-17 12:28:36 +0200

    recyclerview

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinapp.adapter.WordListAdapter
 
 //tut: https://developer.android.com/codelabs/android-room-with-a-view-kotlin#0
 // ist auch gut um nochmal durchzugehen für weiterführende doku links
@@ -10,5 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
